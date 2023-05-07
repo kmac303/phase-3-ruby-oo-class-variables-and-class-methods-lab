e5f789e9063f3046800cae140f76b1784f759205
support <support@kmcintosh-6782.remax.local>
2023-05-07 15:10:20 -0600

    commit tree 1019f672a9e3f8cddd26b3bd504ca1c4dc9a2c14

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,7 @@ class Song
     attr_accessor :name, :artist, :genre
 
     @@count = 0
-
     @@genres = []
-
     @@artists = []
 
     def initialize(name, artist, genre)
@@ -16,7 +14,7 @@ class Song
         @@artists << artist
     end
 
-    def self.count
+    def self.count 
         @@count
     end
 
@@ -27,29 +25,13 @@ class Song
     def self.artists
         @@artists.uniq
     end
-    
+
     def self.genre_count
-        genre_count = {}
-        @@genres.each do |genre|
-            if genre_count[genre]
-                genre_count[genre] += 1
-            else
-                genre_count[genre] = 1
-            end
-        end
-        genre_count
+        @@genres.tally
     end
 
     def self.artist_count
-        artist_count = {}
-        @@artists.each do |artist|
-            if artist_count[artist]
-                artist_count[artist] += 1
-            else
-                artist_count[artist] = 1
-            end
-        end
-        artist_count
+        @@artists.tally
     end
 
 end
